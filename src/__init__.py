5afcd06ae95e1eb3e22cf1427ec1da11ac9837b7
chidi <huxleyberg@live.com>
2025-07-01 01:00:59 -0300

    description

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,10 @@ from src.books.routes import book_router
 
 version = "v1"
 
-app = FastAPI(version=version)
+app = FastAPI(
+    title="Bookly",
+    description="A REST API for a book review web service",
+    version=version,
+)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
