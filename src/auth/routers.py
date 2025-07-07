b5e0d1225ace063421dcef4fa46d64cb8f5284d7
chidi <huxleyberg@live.com>
2025-07-07 11:31:20 -0300

    create_user_account

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,8 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.auth.schemas import UserCreateModel
+from src.auth.service import UserService
+from src.db.main import get_session
 
 auth_router = APIRouter()
+user_service = UserService()
 
 
-@auth_router.post("/signup")
-async def create_user_account():
-    pass
+@auth_router.post("/signup", status_code=status.HTTP_201_CREATED)
+async def create_user_account(
+    user_data: UserCreateModel,
+    session: AsyncSession = Depends(get_session),
+):
+    """
+    Create user account using email, username, first_name, last_name
+    params:
+        user_data: UserCreateModel
+    """
+    email = user_data.email
+
+    user_exists = await user_service.user_exists(email, session)
+
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User with email already exists",
+        )
+
+    new_user = await user_service.create_user(user_data, session)
+
+    return {
+        "message": "Account Created.",
+        "user": new_user,
+    }
