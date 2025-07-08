abe22bd18729c3ec13860f68d5f6cd48020a7eb9
chidi <huxleyberg@live.com>
2025-07-08 15:47:25 -0300

    remove init_db & life_span

--- src/__init__.py
+++ src/__init__.py
@@ -1,27 +1,8 @@
-"""app main entry point"""
-
-from contextlib import asynccontextmanager
-
 import debugpy
-from fastapi import FastAPI, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from fastapi import FastAPI
 
 from src.auth.routers import auth_router
 from src.books.routes import book_router
-from src.db.main import init_db
-
-
-@asynccontextmanager
-async def life_span(_app: FastAPI):
-    """books async context manager"""
-    print("server is starting ..... ")
-    await init_db()
-    yield
-    print("server has been stopped.")
-
 
 VERSION = "v1"
 
@@ -33,7 +14,6 @@ app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
     version=VERSION,
-    lifespan=life_span,
 )
 
 app.include_router(auth_router, prefix=f"/api/{VERSION}/auth", tags=["auth"])
--- src/db/main.py
+++ src/db/main.py
@@ -8,11 +8,6 @@ from src.config import Config
 async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 
 
-async def init_db() -> None:
-    async with async_engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
 async def get_session() -> AsyncSession:
 
     Session = sessionmaker(
