cc16051dfacdf745e61d06069b5fe33025ac304e
chidi <huxleyberg@live.com>
2025-07-12 04:04:58 -0300

    middleware

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import debugpy
 from fastapi import FastAPI
 
+from middleware import register_middleware
 from src.auth.routers import auth_router
 from src.books.routes import book_router
 from src.errors import register_all_errors
@@ -20,6 +21,8 @@ app = FastAPI(
 )
 
 register_all_errors(app)
+
+register_middleware(app)
 
 app.include_router(auth_router, prefix=f"/api/{VERSION}/auth", tags=["auth"])
 app.include_router(book_router, prefix=f"/api/{VERSION}/books", tags=["books"])
