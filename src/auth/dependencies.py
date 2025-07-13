cc83d10996ba9bd2e7db721b3ea21b4a937a5cd9
chidi <huxleyberg@live.com>
2025-07-13 14:23:22 -0300

    user account verification

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -83,8 +83,8 @@ class RoleChecker:
         self.allowed_roles = allowed_roles
 
     def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
-        # if not current_user.is_verified:
-        #     raise AccountNotVerified()
+        if not current_user.is_verified:
+            raise AccountNotVerified()
         if current_user.role in self.allowed_roles:
             return True
 
